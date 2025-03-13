42100fd32e62406ea66c35fdc1219e9b1a093bf2
ANIK365PRO <mohonto05@gmail.com>
2025-03-13 19:43:26 +0600

    added index.js file and create loadCategories function

---
+++ script/index.js
@@ -0,0 +1,13 @@
+function loadCategories() {
+  // fetch data
+
+  fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(response => response.json())
+    .then(data => displayCategories(data.categories));
+}
+
+function displayCategories(categories) {
+  console.log(categories);
+}
+
+loadCategories();
