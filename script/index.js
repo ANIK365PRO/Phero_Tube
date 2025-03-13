e997f76d2ca8f52197a93396b57a2aa000c7c386
ANIK365PRO <mohonto05@gmail.com>
2025-03-13 22:55:16 +0600

    added displayCategories functon for dynamic button

--- script/index.js
+++ script/index.js
@@ -1,13 +1,35 @@
 function loadCategories() {
-  // fetch data
-
+  //* fetch the data
   fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    //* convert promise to json
     .then(response => response.json())
+
+    //* send data to display
     .then(data => displayCategories(data.categories));
 }
 
+// {
+//     "category_id": "1001",
+//     "category": "Music"
+// }
+
 function displayCategories(categories) {
-  console.log(categories);
+  // console.log(categories);
+  //* get the container
+  const categoriesContainer = document.getElementById('categories_container');
+
+  //* loop operation on Array of object
+  for (let cat of categories) {
+    console.log(cat);
+    //* create Element
+    const categoryDiv = document.createElement('div');
+    categoryDiv.innerHTML = `
+    <button class="btn btn-sm text-lg hover:bg-[#FF1F3D] hover:text-white hover:font-bold">${cat.category}</button>
+    `;
+
+    //* append Element
+    categoriesContainer.appendChild(categoryDiv);
+  }
 }
 
 loadCategories();
